e386de2b746faa17034e6191888f9cac9bb1ccf3
Isa <168196432+Isaa28@users.noreply.github.com>
2025-05-27 09:02:43 -0300

    Modificação das mensagens de erro.

--- codigo-fonte/form-cadastro.php
+++ codigo-fonte/form-cadastro.php
@@ -2,28 +2,30 @@
         try {
             require_once 'conexao.php';
             if(isset($_POST['nome-empresa'], $_POST['cnpj'], $_POST['senha'])){
-                $nomeEmpresa = $_POST['nome-empresa'];
-                $cnpj = $_POST['cnpj'];
-                $senha = $_POST['senha'];
-                $stmt = $conexao->prepare(query:'INSERT INTO usuario (Nome_da_empresa, cnpj, Senha) VALUES (:nome, :cnpj, :senha)');
-                $stmt->bindValue(':nome', $nomeEmpresa); 
-                $stmt->bindValue(':cnpj', $cnpj);
-                $stmt->bindValue(':senha', $senha);
-                if($stmt->execute()){
-                    if($stmt->rowCount() > 0){
-                        $id = null;
-                        $nomeEmpresa = null;
-                        $cnpj = null;
-                        $senha = null;
-                        header("Location: tela-inicial.php");
+                $nomeEmpresa = trim($_POST['nome-empresa']);
+                $cnpj = trim($_POST['cnpj']);
+                $senha = trim($_POST['senha']);
+                if(!empty($nomeEmpresa) && !empty($cnpj) && !empty($senha)){
+                    $stmt = $conexao->prepare(query:'INSERT INTO usuario (Nome_da_empresa, cnpj, Senha) VALUES (:nome, :cnpj, :senha)');
+                    $stmt->bindValue(':nome', $nomeEmpresa); 
+                    $stmt->bindValue(':cnpj', $cnpj);
+                    $stmt->bindValue(':senha', $senha);
+                    if($stmt->execute()){
+                        if($stmt->rowCount() > 0){
+                            $id = null;
+                            $nomeEmpresa = null;
+                            $cnpj = null;
+                            $senha = null;
+                            header("Location: tela-inicial.php");
+                        }else{
+                            echo 'Erro ao tentar efetivar cadastro';
+                        }
                     }else{
-                        echo 'Erro ao tentar efetivar cadastro';
+                        throw new PDOException("Erro: Não foi possível executar a declaração sql");
                     }
                 }else{
-                    throw new PDOException("Erro: Não foi possível executar a declaração sql");
+                    echo '[ERRO]Dados não encontrados.';
                 }
-            }else{
-                echo '[ERRO]Dados não encontrados.';
             }
         }catch (PDOException $erro) {
             echo "Erro: " . $erro->getMessage();
