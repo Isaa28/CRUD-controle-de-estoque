b92e6388994fe52083427c98289e6f75db4e2bba
Isa <168196432+Isaa28@users.noreply.github.com>
2025-05-27 14:53:45 -0300

    Atualização dos códigos e organização dos arquivos

---
+++ cadastro.php
@@ -0,0 +1,65 @@
+<?php
+    require_once 'conexao.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Astok</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/cadastro-usuario.css">
+</head>
+<body>
+    <div id="tela-de-cadastro">
+        <div id="form-cadastro">
+            <h1>Faça seu <br> cadastro</h1>
+            <?php
+                try {
+                    if(isset($_POST['nome-empresa'], $_POST['cnpj'], $_POST['senha'])){
+                        $nomeEmpresa = trim($_POST['nome-empresa']);
+                        $cnpj = trim($_POST['cnpj']);
+                        $senha = trim($_POST['senha']);
+                        if(!empty($nomeEmpresa) && !empty($cnpj) && !empty($senha)){
+                            $stmt = $conexao->prepare(query:'INSERT INTO usuario (Nome_da_empresa, cnpj, Senha) VALUES (:nome, :cnpj, :senha)');
+                            $stmt->bindValue(':nome', $nomeEmpresa); 
+                            $stmt->bindValue(':cnpj', $cnpj);
+                            $stmt->bindValue(':senha', $senha);
+                            if($stmt->execute()){
+                                if($stmt->rowCount() > 0){
+                                    $id = null;
+                                    $nomeEmpresa = null;
+                                    $cnpj = null;
+                                    $senha = null;
+                                }else{
+                                    echo "<div class='erro'>Erro ao tentar efetivar cadastro.</div>";
+                                }
+                            }else{
+                                throw new PDOException("Erro: Não foi possível executar a declaração sql");
+                            }
+                        }else{
+                                echo "<div class='erro'>Confira se todos os dados foram preenchidos.</div>";
+                        }
+                    }
+                }catch (PDOException $erro) {
+                    echo "Erro: " . $erro->getMessage();
+                }
+            ?>
+            <form action="" method="post">
+            <label class="rotulo" for="nome-empresa">Nome da empresa:</label>
+            <input type="text" name="nome-empresa" class="estilo-input" placeholder="Ex: Mecardinho Ramos">
+            <label class="rotulo" for="cnpj">CNPJ:</label>
+            <input type="text" name="cnpj" class="estilo-input" placeholder="Ex: 12.345.678/0001-95">
+            <label class="rotulo" for="senha">Senha:</label>
+            <input type="password" name="senha" class="estilo-input" placeholder="Ex: 78364724289">
+            <input type="submit" value="Cadastre-se" class="botoes">
+            </form>
+        </div>
+        <div id="imagem-lateral">
+            <img id="imagem-login" src="assets/imagens/imagem-login.png" alt="">
+            <img id="logo" src="assets/imagens/logo.png" alt="Logo Astok">
+        </div>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Astok</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+
+    <div id="tela-de-login">
+        <div id="login">
+            <h1>Login</h1>
+
+            <?php
+            require_once "conexao.php";
+
+            session_start();
+
+            if (isset($_POST["cnpj"], $_POST["senha"])) {
+                $cnpj = trim($_POST["cnpj"]);
+                $senha = trim($_POST["senha"]);
+
+                if (empty($cnpj)) {
+                    echo "<div class='erro'>Preencha seu CNPJ </div>";
+                } elseif (empty($senha)) {
+                    echo "<div class='erro'>Preencha sua senha </div>";
+                } else {
+                    try {
+                        $stmt = $conexao->prepare("SELECT * FROM usuario WHERE cnpj = :cnpj");
+                        $stmt->bindValue(":cnpj", $cnpj, PDO::PARAM_STR);
+                        $stmt->execute();
+
+                        $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                        if ($usuario) {
+                            if (!isset($usuario["Senha"])) {
+                                echo "<div class='erro'>Erro: campo 'Senha' não encontrado no banco de dados.</div>";
+                                exit();
+                            }
+
+                            if ($senha === $usuario["Senha"]) {
+                                $_SESSION["id"] = $usuario["ID"];
+                                $_SESSION["Nome_da_empresa"] = $usuario["Nome_da_empresa"];
+
+                                header("Location: tela-inicial.php");
+                                exit();
+                            } else {
+                                echo "<div class='erro'>Senha ou cnpj incorretos.</div>";
+                            }
+                        } else {
+                            echo "<div class='erro'>CNPJ não encontrado.</div>";
+                        }
+                    } catch (PDOException $e) {
+                        echo "Erro: " . $e->getMessage();
+                    } 
+                }
+            }
+            ?>
+
+            <form action="" method="post">
+                <label class="rotulo" for="cnpj">CNPJ:</label>
+                <input type="text" name="cnpj" class="estilo-input" placeholder="Ex: 01.234.567/0001-89">
+                <label class="rotulo" for="senha">Senha:</label>
+                <input type="password" name="senha" class="estilo-input" placeholder="Ex: MinhaSenha123!">
+                <input type="submit" value="Entrar" class="botoes">
+            </form>
+
+            <div id="div-link">
+                <p>Não tem conta?<a id="link-cadastro" href="cadastro.php">Cadastre-se</a></p>  
+            </div>
+        </div>
+                
+        <div id="imagem-lateral">
+            <img id="imagem-login" src="assets/imagens/imagem-login.png" alt="">
+            <img id="logo" src="assets/imagens/logo.png" alt="Logo Astok">
+        </div>
+    </div>
+</body>
+</html>
