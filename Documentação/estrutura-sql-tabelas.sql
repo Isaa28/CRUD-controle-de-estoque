3a9f7e8b78b6765310733a9c4e033e61cb1bbb67
Isa <168196432+Isaa28@users.noreply.github.com>
2025-06-20 12:48:51 -0300

    Criando a tabela associativa de fornecedores e usuários e atualizando as tabelas.

--- Documentação/estrutura-sql-tabelas.sql
+++ Documentação/estrutura-sql-tabelas.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE estoque; 
+CREATE DATABASE estoque;
 USE estoque;
 
-CREATE TABLE Usuario(
+CREATE TABLE Usuario (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome_da_empresa VARCHAR(100) NOT NULL,
     cnpj VARCHAR(18) NOT NULL UNIQUE,
@@ -10,7 +10,11 @@ CREATE TABLE Usuario(
 
 CREATE TABLE Categorias (
     ID INT AUTO_INCREMENT PRIMARY KEY,
-    Nome_categoria VARCHAR(100) NOT NULL UNIQUE
+    Nome_categoria VARCHAR(100) NOT NULL UNIQUE,
+    Usuario_ID INT NOT NULL,
+    CONSTRAINT fk_categoria_usuario
+        FOREIGN KEY (Usuario_ID) REFERENCES Usuario(ID)
+        ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE Fornecedores (
@@ -22,6 +26,18 @@ CREATE TABLE Fornecedores (
     Endereco VARCHAR(100)
 );
 
+CREATE TABLE fornecedor_usuario (
+    Fornecedor_ID INT NOT NULL,
+    Usuario_ID INT NOT NULL,
+    CONSTRAINT fk_fornecedor_usuario_usuario
+        FOREIGN KEY (Usuario_ID) REFERENCES Usuario(ID)
+        ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_fornecedor_usuario_fornecedor
+        FOREIGN KEY (Fornecedor_ID) REFERENCES Fornecedores(ID)
+        ON DELETE RESTRICT ON UPDATE CASCADE,
+    PRIMARY KEY (Fornecedor_ID, Usuario_ID)
+);
+
 CREATE TABLE Produtos (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Nome_produto VARCHAR(100) NOT NULL,
@@ -29,15 +45,19 @@ CREATE TABLE Produtos (
     Preco DECIMAL(10,2) NOT NULL,
     Fornecedor_ID INT NOT NULL,
     Categoria_ID INT NOT NULL,
+    Usuario_ID INT NOT NULL,
     CONSTRAINT fk_produto_fornecedor
-	FOREIGN KEY (Fornecedor_ID) REFERENCES Fornecedores(ID)
-	ON DELETE RESTRICT ON UPDATE CASCADE,
+        FOREIGN KEY (Fornecedor_ID) REFERENCES Fornecedores(ID)
+        ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT fk_produto_categoria
-	FOREIGN KEY (Categoria_ID) REFERENCES Categorias(ID)
-	ON DELETE RESTRICT ON UPDATE CASCADE
+        FOREIGN KEY (Categoria_ID) REFERENCES Categorias(ID)
+        ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_produto_usuario
+        FOREIGN KEY (Usuario_ID) REFERENCES Usuario(ID)
+        ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
-CREATE TABLE Entrada (Add commentMore actions
+CREATE TABLE Entrada (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     data DATE NOT NULL,
     Produto_ID INT NOT NULL,
